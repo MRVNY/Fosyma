06680ac2193f3ff3f3b16ec63a1ca1dbe14b86e8
MRVNY <MRVNY@users.noreply.github.com>
2022-02-22 12:37:42 +0100

    FSM

---
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/FSM/FSMExplo.java
@@ -0,0 +1,129 @@
+package eu.su.mas.dedaleEtu.mas.behaviours.FSM;
+
+import dataStructures.serializableGraph.SerializableSimpleGraph;
+import dataStructures.tuple.Couple;
+import eu.su.mas.dedale.env.Observation;
+import eu.su.mas.dedale.mas.AbstractDedaleAgent;
+import eu.su.mas.dedaleEtu.mas.behaviours.ShareMapBehaviour;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation.MapAttribute;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+
+import java.util.Iterator;
+import java.util.List;
+
+
+/**
+ * <pre>
+ * This behaviour allows an agent to explore the environment and learn the associated topological map.
+ * The algorithm is a pseudo - DFS computationally consuming because its not optimised at all.
+ * 
+ * When all the nodes around him are visited, the agent randomly select an open node and go there to restart its dfs. 
+ * This (non optimal) behaviour is done until all nodes are explored. 
+ * 
+ * Warning, this behaviour does not save the content of visited nodes, only the topology.
+ * Warning, the sub-behaviour ShareMap periodically share the whole map
+ * </pre>
+ * @author hc
+ *
+ */
+public class FSMExplo extends SimpleBehaviour {
+
+	private static final long serialVersionUID = 8567689731496787661L;
+
+	private boolean finished = false;
+
+	/**
+	 * Current knowledge of the agent regarding the environment
+	 */
+	private MapRepresentation myMap;
+
+	private List<String> list_agentNames;
+
+/**
+ *
+ * @param myagent
+ * @param myMap known map of the world the agent is living in
+ * @param agentNames name of the agents to share the map with
+ */
+	public FSMExplo(final AbstractDedaleAgent myagent, MapRepresentation myMap, List<String> agentNames) {
+		super(myagent);
+		this.myMap=myMap;
+		this.list_agentNames=agentNames;
+		
+		
+	}
+
+	@Override
+	public void action() {
+
+		if(this.myMap==null) {
+			this.myMap= new MapRepresentation();
+			this.myAgent.addBehaviour(new ShareMapBehaviour(this.myAgent,500,this.myMap,list_agentNames));
+		}
+
+		//0) Retrieve the current position
+		String myPosition=((AbstractDedaleAgent)this.myAgent).getCurrentPosition();
+
+		if (myPosition!=null){
+			//List of observable from the agent's current position
+			List<Couple<String,List<Couple<Observation,Integer>>>> lobs=((AbstractDedaleAgent)this.myAgent).observe();//myPosition
+
+			/**
+			 * Just added here to let you see what the agent is doing, otherwise he will be too quick
+			 */
+			try {
+				this.myAgent.doWait(1000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			//1) remove the current node from openlist and add it to closedNodes.
+			this.myMap.addNode(myPosition, MapAttribute.closed);
+
+			//2) get the surrounding nodes and, if not in closedNodes, add them to open nodes.
+			String nextNode=null;
+			Iterator<Couple<String, List<Couple<Observation, Integer>>>> iter=lobs.iterator();
+			while(iter.hasNext()){
+				String nodeId=iter.next().getLeft();
+				boolean isNewNode=this.myMap.addNewNode(nodeId);
+				//the node may exist, but not necessarily the edge
+				if (myPosition!=nodeId) {
+					this.myMap.addEdge(myPosition, nodeId);
+					if (nextNode==null && isNewNode) nextNode=nodeId;
+				}
+			}
+
+			//3) while openNodes is not empty, continues.
+			if (!this.myMap.hasOpenNode()){
+				//Explo finished
+				finished=true;
+				System.out.println(this.myAgent.getLocalName()+" - Exploration successufully done, behaviour removed.");
+			}else{
+				//4) select next move.
+				//4.1 If there exist one open node directly reachable, go for it,
+				//	 otherwise choose one from the openNode list, compute the shortestPath and go for it
+				if (nextNode==null){
+					//no directly accessible openNode
+					//chose one, compute the path and take the first step.
+					nextNode=this.myMap.getShortestPathToClosestOpenNode(myPosition).get(0);//getShortestPath(myPosition,this.openNodes.get(0)).get(0);
+					//System.out.println(this.myAgent.getLocalName()+"-- list= "+this.myMap.getOpenNodes()+"| nextNode: "+nextNode);
+				}else {
+					//System.out.println("nextNode notNUll - "+this.myAgent.getLocalName()+"-- list= "+this.myMap.getOpenNodes()+"\n -- nextNode: "+nextNode);
+				}
+
+
+				((AbstractDedaleAgent)this.myAgent).moveTo(nextNode);
+			}
+		}
+	}
+
+	@Override
+	public boolean done() {
+		return finished;
+	}
+
+}
