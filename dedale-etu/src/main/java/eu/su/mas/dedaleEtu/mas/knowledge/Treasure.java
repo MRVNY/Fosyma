0bb7d29afc6592793bb8b73371e376320737f9ba
White-On <74248238+White-On@users.noreply.github.com>
2022-04-12 08:50:21 +0200

    update adding treasures to map

---
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/knowledge/Treasure.java
@@ -0,0 +1,39 @@
+package eu.su.mas.dedaleEtu.mas.knowledge;
+
+public class Treasure {
+		
+	
+	public enum TypeTreasure {	
+		GOLD,DIAMOND;
+
+	}
+	
+	private int treasureAmount;
+	// Id of the Node
+	private String location;
+	private TypeTreasure type;
+	
+	public Treasure(int treasureAmount, String Location, TypeTreasure type) {
+		this.treasureAmount = treasureAmount;
+		this.location = location;
+		this.type = type;
+	}
+
+	public int getTreasureAmount() {
+		return treasureAmount;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public TypeTreasure getType() {
+		return type;
+	}
+	
+	public String toString() {
+		return "("+this.type+";"+this.treasureAmount+")";
+	}
+	
+
+}
